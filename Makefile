8a14784c10ed70cf89f418ef1102d890771782f9
Steven Haid <stevenhaid@gmail.com>
2021-08-01 10:01:44 -0400

    start to add graphics

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
-tides: tides.c
-	gcc -g -Wall -O2 -I. $^ -lm -o $@
+
+tides: main.c util_sdl.c
+	gcc -g -Wall -O2 -I. -I/usr/include/SDL2 $^ -lm -lSDL2 -lSDL2_ttf -o $@
 
 clean:
 	rm -f tides
--- tides.c
+++ tides.c
@@ -1,11 +1,13 @@
+// xxx more comments needed
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <math.h>
-
-// This program calculates the location and height of the Earth's tidal bulges,
-// caused by the Moon. XXX refer to notes.txt
+#include <pthread.h>
+#include <assert.h>
 
 //
 // defines
@@ -18,12 +20,14 @@
 #define G                  6.67408e-11      // gravitational constant
 #define EARTH_GRAVITY      9.81             // gravity of earth, m/s/s
 
-#define METERS_TO_MILES(m) ((m) * 0.000621371)
-#define TWO_PI             (2 * M_PI)
-#define DEG_TO_RAD(d)      ((d) * (M_PI/180))
-#define RAD_TO_DEG(r)      ((r) * (180/M_PI))
+#define DELTA_R            1e-3
+#define MAX_EARTH_SURFACE  100000
 
-#define DELTA_H            1e-3
+#define METERS_TO_MILES(m)          ((m) * 0.000621371)
+#define METERS_TO_NAUTICAL_MILES(m) ((m) * 0.000539957)
+#define TWO_PI                      (2 * M_PI)
+#define DEG_TO_RAD(d)               ((d) * (M_PI/180))
+#define RAD_TO_DEG(r)               ((r) * (180/M_PI))
 
 //
 // typedefs
@@ -54,38 +58,42 @@ struct {
     double mass;
     double radius;
     double w;
-    double g[100000];  // xxx rename g_surface
-    double h[100000];  // xxx rename r
     double tpe;
+    struct {
+        double g;
+        double r;
+        double x;
+        double y;
+        double z;
+    } surface[MAX_EARTH_SURFACE];
+    int max_surface;
 } earth;
-
-struct {
-    double x;
-    double y;
-    double z;
-} xyz[100000];  // xxx malloc ?
-int max_xyz;
 
 //
 // prototypes
 //
 
-static void init_xyz(void);
-static double potential_energy(double m, double g_surface, double r);
+static void tides_init(void);
+static void init_earth_surface_xyz(void);
+
+static void *tides_thread(void *cx);
+
 static double square(double x);
 static double magnitude(vector_t *v);
 static int set_vector_magnitude(vector_t *v, double new_magnitude);
 
-// ---------------------------------------------------
+// -----------------  INIT  --------------------------
 
-int main(int argc, char **argv)
+static void tides_init(void)
 {
+    pthread_t tid;
+
     // Init constant fields of the earth and moon structs.
     earth.mass   = EARTH_MASS;
     moon.mass    = MOON_MASS;
     earth.radius = EARTH_RADIUS;
     for (int i = 0; i < 100000; i++) {
-        earth.h[i] = EARTH_RADIUS;
+        earth.surface[i].r = EARTH_RADIUS;
     }
 
     // Determine the position of the earth and moon relative to the center of mass.
@@ -108,213 +116,149 @@ int main(int argc, char **argv)
     double gravity_force = G * earth.mass * moon.mass / (DIST_EARTH_MOON * DIST_EARTH_MOON);
     moon.w  = sqrt(gravity_force / (moon.mass * fabs(moon.x)));
     earth.w = sqrt(gravity_force / (earth.mass * fabs(earth.x)));
-    // xxx sanity check they are within .0000001
-    printf("moon.w         = %e\n", moon.w);
-    printf("earth.w        = %e\n", earth.w);
-    printf("orbital period = %f days\n", TWO_PI / moon.w / 86400);
+    assert(fabs(moon.w / earth.w - 1) < 1e-10);
+    printf("moon.w          = %0.6e\n", moon.w);
+    printf("earth.w         = %0.6e\n", earth.w);
+    printf("orbital period  = %f days\n", TWO_PI / moon.w / 86400);
     printf("\n");
 
-    //xxx
-    init_xyz();
-    //max_xyz = 360; //xxx
+    // init array of roughly evenly distributed locations on the earth surface
+    init_earth_surface_xyz();
 
-    // xxx comment
-    for (int i = 0; i < max_xyz; i++) {
-        double x, y, z, d;
+    // compute accel vectors for all earth surface locations just computed
+    // - g = earth gravity
+    // - m = moon gravity
+    // - c = centrifugal accel
+    // - t = g + m + c
+    for (int i = 0; i < earth.max_surface; i++) {
         vector_t g, m, c, t;
 
-        x = xyz[i].x;  // xxx elim var x,y,z
-        y = xyz[i].y;
-        z = xyz[i].z;
-
-        g.a = earth.x - x;
-        g.b = earth.y - y;
-        g.c = earth.z - z;
+        g.a = earth.x - earth.surface[i].x;
+        g.b = earth.y - earth.surface[i].y;
+        g.c = earth.z - earth.surface[i].z;
         set_vector_magnitude(&g, EARTH_GRAVITY);
 
-        m.a = moon.x - x;
-        m.b = moon.y - y;
-        m.c = moon.z - z;
-        d = magnitude(&m);
+        m.a = moon.x - earth.surface[i].x;
+        m.b = moon.y - earth.surface[i].y;
+        m.c = moon.z - earth.surface[i].z;
+        double d = magnitude(&m);
         set_vector_magnitude(&m, G * moon.mass / square(d));
 
-#if 1
         c.a = -1;
         c.b = 0;
         c.c = 0;
         set_vector_magnitude(&c,  square(earth.w) * -earth.x);
-#endif
-#if 0
-        c.a = x;
-        c.b = y;
-        c.c = z;
-        d = magnitude(&c);
-        set_vector_magnitude(&c, square(earth.w) * d);
-#endif
-#if 0
-        memset(&c,0,sizeof(c));
-#endif
 
         t.a = g.a + m.a + c.a;
         t.b = g.b + m.b + c.b;
         t.c = g.c + m.c + c.c;
-        earth.g[i] = magnitude(&t);
+        earth.surface[i].g = magnitude(&t);
 
         if (i == 0 || i == 90 || i == 180 || i == 270) {
-            printf("%3d: earth gravity     = %0.9f %0.9f %0.9f\n", i, g.a, g.b, g.c);
-            printf("     moon gravity      = %0.9f %0.9f %0.9f\n", m.a, m.b, m.c);
-            printf("     centrifugal accel = %0.9f %0.9f %0.9f\n", c.a, c.b, c.c);
-            printf("     total             = %0.9f %0.9f %0.9f\n", t.a, t.b, t.c);
-            printf("     MAGNITUDE         = %0.9f\n", earth.g[i]);
-            printf("\n");
+            if (i == 0) printf("accel vectors:\n");
+            printf("%3d: earth gravity     = %+0.9f %+0.9f %+0.9f\n", i, g.a, g.b, g.c);
+            printf("     moon gravity      = %+0.9f %+0.9f %+0.9f\n", m.a, m.b, m.c);
+            printf("     centrifugal accel = %+0.9f %+0.9f %+0.9f\n", c.a, c.b, c.c);
+            printf("     total             = %+0.9f %+0.9f %+0.9f\n", t.a, t.b, t.c);
+            printf("     MAGNITUDE         = %0.9f\n", earth.surface[i].g);
+            if (i == 270) printf("\n");
         }
     }
 
-    printf("Running ...\n");
-    int loops = 0;
-    double earth_tpe_last = 0;
-    //double m;
-    int tpe_unchanged_count = 0;
-    while (true) {
-        int i = random() % max_xyz;
-        int j = random() % max_xyz;
-        double delta_pe;
-        if (i == j) continue;
-
-#if 0
-        // xxx use this one and clean up the mass calc
-        delta_pe = 0;
-        m = square((earth.h[i]+(DELTA_H/2)) / earth.radius) * DELTA_H,
-        delta_pe += potential_energy(m,
-                                     earth.g[i], 
-                                     earth.h[i]+(DELTA_H/2));
-        m = square((earth.h[j]-(DELTA_H/2)) / earth.radius) * DELTA_H,
-        delta_pe -= potential_energy(m,
-                                     earth.g[j], 
-                                     earth.h[j]-(DELTA_H/2));
-#else
-        // don't need a routine
-        delta_pe = 0;
-        delta_pe += potential_energy(square(earth.h[i]+(DELTA_H/2)),
-                                     earth.g[i], 
-                                     earth.h[i]+(DELTA_H/2));
-        delta_pe -= potential_energy(square(earth.h[j]-(DELTA_H/2)),
-                                     earth.g[j], 
-                                     earth.h[j]-(DELTA_H/2));
-#endif
-
-        if (delta_pe < 0) {
-            earth.h[i] += DELTA_H;
-            earth.h[j] -= DELTA_H;
-            earth.tpe += delta_pe;
-        }
-
-        loops++;
-        if ((loops % 10000) == 0) {
-            //printf("   loops = %9d   tpe = %0.10e   tpe_unchanged_count = %d\n", loops, earth.tpe, tpe_unchanged_count);
-            if (earth.tpe == earth_tpe_last) {
-                tpe_unchanged_count++;
-                // AAAA XXX
-                if (tpe_unchanged_count == 10) break;
-            } else {
-                tpe_unchanged_count = 0;
-            }
-            earth_tpe_last = earth.tpe;
-        }
-        if (loops == 50000000) break;
-    }
-  
-    // print results
-    printf("Results ...\n");
-    double minh=1e99, maxh=-1e99;
-    for (int i = 0; i < max_xyz; i++) {
-        double h = earth.h[i];
-        if (i == 0 || i == 90 || i == 180 || i == 270) {
-            printf("   deg = %3d   height = %0.10f  height-earth_radius = %+0.6f\n", 
-                   i, h, h-earth.radius);
-        }
-// XXX print out which is min and max
-        if (h < minh) minh = h;
-        if (h > maxh) maxh = h;
-    }
-    printf("\n");
-    printf("   min = %f max = %f  Range = %f\n", minh, maxh, maxh-minh);
-
-    // done
-    return 0;
+    // create runtime thread
+    pthread_create(&tid, NULL, tides_thread, NULL);
 }
 
-// -----------------  XXXXXXXXXXXXXXXX  -----------------------------
-
-static void init_xyz(void)
+static void init_earth_surface_xyz(void)
 {
-//x = earth.x + earth.radius * cos(DEG_TO_RAD(i));
-//y = earth.y + earth.radius * sin(DEG_TO_RAD(i));
-// XXX deg vs rad
-// xxx add earth.x etc   XXX
+    #define ADD(_x,_y,_z) \
+        do { \
+            int idx = earth.max_surface; \
+            assert(idx < MAX_EARTH_SURFACE); \
+            earth.surface[idx].x = earth.x + (_x); \
+            earth.surface[idx].y = earth.y + (_y); \
+            earth.surface[idx].z = earth.z + (_z); \
+            earth.max_surface++; \
+        } while (0)
 
     double size, latitude, longitude, circ, x, y, z;
     int n, i;
 
     size = TWO_PI * EARTH_RADIUS / 360;
-    printf("size = %.0f\n", size);
+    printf("init_earth_surface:\n");
+    printf("  size = %.0f m   %0.6f NauticalMiles\n", size, METERS_TO_NAUTICAL_MILES(size));
 
-    for (latitude = 0; latitude < 90; latitude += 1) {
+    for (latitude = 0; latitude <= 90; latitude += 1) {
         circ = TWO_PI * EARTH_RADIUS * cos(DEG_TO_RAD(latitude));
         if (latitude < 90) {
             n = nearbyint(circ / size);
         } else {
             n = nearbyint((M_PI * square(EARTH_RADIUS * sin(DEG_TO_RAD(0.5)))) / square(size));
-            printf("XXXXXXXXXXXXXXXXX OOPS\n");
         }
-        printf("lat = %.0f  circ = %.0f  n = %d\n", latitude, circ, n);
+        //printf("  latitude = %.0f  circ = %.0f  n = %d\n", latitude, circ, n);
+
         for (i = 0; i < n; i++) {
             longitude = i * (360./n);
             x = EARTH_RADIUS * cos(DEG_TO_RAD(latitude)) * cos(DEG_TO_RAD(longitude));
             y = EARTH_RADIUS * cos(DEG_TO_RAD(latitude)) * sin(DEG_TO_RAD(longitude));
             z = EARTH_RADIUS * sin(DEG_TO_RAD(latitude));
 
-            if (latitude == 0) {
-                printf("  longitude = %.3f  xyz = %10.0f %10.0f %10.0f\n", longitude, x, y, z);
-            }
+            //if (latitude == 0) {
+            //    printf("    longitude = %8.3f  xyz = %10.0f %10.0f %10.0f\n", longitude, x, y, z);
+            //}
 
-            // xyz =
             if (latitude == 0) {
-                xyz[max_xyz].x = earth.x + x;
-                xyz[max_xyz].y = earth.y + y;
-                xyz[max_xyz].z = earth.z + z;
-                max_xyz++;
+                ADD(x,y,z);
             } else {
-                xyz[max_xyz].x = earth.x + x;
-                xyz[max_xyz].y = earth.y + y;
-                xyz[max_xyz].z = earth.z + z;
-                max_xyz++;
-
-                xyz[max_xyz].x = earth.x + x;
-                xyz[max_xyz].y = earth.y + y;
-                xyz[max_xyz].z = earth.z + -z;
-                max_xyz++;
+                ADD(x,y,z);
+                ADD(x,y,-z);
             }
         }
     }
-    printf("max_xyz = %d\n", max_xyz);
-    printf("  %e  %e\n",
-           max_xyz * square(size),
-           4 * M_PI * square(EARTH_RADIUS));
+
+    printf("  earth.max_surface = %d\n", earth.max_surface);
+    printf("    %e  %e\n", earth.max_surface * square(size), 4 * M_PI * square(EARTH_RADIUS));
+    printf("\n");
 }
 
-// -----------------  POTENTIAL ENERGY  -----------------------------
+// -----------------  RUNTIME  -----------------------
 
-// xxx maybe this doesnt need to be a routine
-static double potential_energy(double m, double g_surface, double r)
+static void *tides_thread(void *cx)
 {
-    // xxx change r to R  or change comments above to use 'r'
-    // xxx use 0.5L ?
-    //return 0.5 * m * (g_surface / EARTH_RADIUS) * (r * r);
-    return m * g_surface * r * r;
+    int loops __attribute__((unused)) = 0;
+
+    while (true) {
+        int i = random() % earth.max_surface;
+        int j = random() % earth.max_surface;
+        double delta_pe=0, m, g_surface, r;
+
+        if (i == j) continue;
+
+        m         = square(earth.surface[i].r + (DELTA_R/2));
+        g_surface = earth.surface[i].g;
+        r         = earth.surface[i].r + (DELTA_R/2);
+        delta_pe += m * g_surface * square(r);
+
+        m         = square(earth.surface[j].r - (DELTA_R/2));
+        g_surface = earth.surface[j].g;
+        r         = earth.surface[j].r - (DELTA_R/2);
+        delta_pe -= m * g_surface * square(r);
+
+        if (delta_pe < 0) {
+            earth.surface[i].r += DELTA_R;
+            earth.surface[j].r -= DELTA_R;
+            earth.tpe += delta_pe;
+        }
+
+        //if ((++loops % 1000000) == 0) {
+        //    printf("   loops = %9d   tpe = %0.9e\n", loops, earth.tpe);
+        //}
+    }
+
+    return NULL;
 }
 
-// -----------------  UTILS  ----------------------------------------
+// -----------------  UTILS  -------------------------
 
 static double square(double x)
 {
@@ -331,9 +275,7 @@ static int set_vector_magnitude(vector_t *v, double new_magnitude)
     double current_magnitude, factor;
 
     current_magnitude = magnitude(v);
-    if (current_magnitude == 0) {
-        return -1;  // xxx or abort
-    }
+    assert(current_magnitude > 0);
 
     factor = new_magnitude / current_magnitude;
 
@@ -343,3 +285,47 @@ static int set_vector_magnitude(vector_t *v, double new_magnitude)
 
     return 0;
 }
+
+// -----------------  UNIT TEST  ---------------------
+
+#ifdef UNIT_TEST
+
+// gcc -Wall -g -O2 -DUNIT_TEST -o tides tides.c -lm -lpthread
+
+int main(int argc, char **argv)
+{
+    tides_init();
+
+    while (true) {
+        sleep(1);
+
+        printf("Results ...\n");
+
+        double min_r=1e99, max_r=-1e99;
+        int min_r_idx=-1, max_r_idx=-1;
+
+        for (int i = 0; i < earth.max_surface; i++) {
+            double r = earth.surface[i].r;
+
+            if (i == 0 || i == 90 || i == 180 || i == 270) {
+                printf("   deg = %3d   height = %0.10f  height-earth_radius = %+0.6f\n", 
+                       i, r, r-earth.radius);
+            }
+
+            if (r < min_r) {
+                min_r = r;
+                min_r_idx = i;
+            }
+            if (r > max_r) {
+                max_r = r;
+                max_r_idx = i;
+            }
+        }
+        printf("   min = %f (idx=%d)   max = %f (idx=%d)   range = %f\n", 
+               min_r, min_r_idx, max_r, max_r_idx, max_r-min_r);
+        printf("\n");
+    }
+
+    return 0;
+}
+#endif
